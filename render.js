2a64706eb2d0b8ed114082d8731ee817ead7bf85
Gungundamdam <holimelonapp@gmail.com>
2025-11-18 10:34:36 +0100

    Update CV - 2025-11-18 10:34:35

--- render.js
+++ render.js
@@ -1,7 +1,13 @@
 const ejs = require('ejs');
 const fs = require('fs');
 
+
 const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+const theme = process.argv.includes('--light') ? 'light' : 'dark';
+
+data.theme = theme;
+
 ejs.renderFile('template.ejs', data, {}, (err, str) => {
     if (err) throw err;
     fs.writeFileSync('index.html', str);
