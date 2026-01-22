e91bf09079422baca8c037f41876ccbf6f52a7b6
Gungundamdam <holimelonapp@gmail.com>
2026-01-22 15:34:01 +0100

    Update CV - 2026-01-22 15:34:01

--- render.js
+++ render.js
@@ -8,18 +8,26 @@ const dataFr = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
 dataFr.theme = theme;
 dataFr.lang = "FR";
+dataFr.pdf = {
+    href:"./pdf/cv_white.pdf",
+    name:"CV_Laurent_Tempier.pdf"
+};
+
 ejs.renderFile('template.ejs', dataFr, {}, (err, str) => {
     if (err) throw err;
     fs.writeFileSync('index.html', str);
     console.log('CV généré');
 });
 
-
-
 const dataEn = JSON.parse(fs.readFileSync('data_en.json', 'utf8'));
 
 dataEn.theme = theme;
 dataEn.lang = "EN";
+dataEn.pdf = {
+    href:"./pdf/cv_white_en.pdf",
+    name:"CV_Laurent_Tempier_EN.pdf"
+};
+
 ejs.renderFile('template.ejs', dataEn, {}, (err, str) => {
     if (err) throw err;
     fs.writeFileSync('en.html', str);
